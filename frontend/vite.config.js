e30134a33b15b9cb81cf9c1fdcc4e966d0a2e9c0
seongs <seongsland@gmail.com>
2026-01-28 19:05:50 +0900

    VITE_API_URL 환경변수 처리 추가

--- frontend/vite.config.js
+++ frontend/vite.config.js
@@ -1,19 +1,21 @@
-import {defineConfig} from 'vite'
+import {defineConfig, loadEnv} from 'vite'
 import react from '@vitejs/plugin-react'
 
-export default defineConfig({
-    plugins: [react()],
-    server: {
-        port: 3000,
-        proxy: {
-            '/api': {
-                target: 'http://localhost:8080',
-                changeOrigin: true
+// https://vitejs.dev/config/
+export default defineConfig(({mode}) => {
+    // 1. env 변수를 여기서 먼저 선언하고 값을 할당합니다.
+    const env = loadEnv(mode, '.', '');
+
+    return {
+        plugins: [react()],
+        server: {
+            port: 3000,
+            proxy: {
+                '/api': {
+                    target: env.VITE_API_URL || 'http://localhost:8080',
+                    changeOrigin: true
+                }
             }
         }
     }
 })
-
-
-
-
