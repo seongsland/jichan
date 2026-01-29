acb33720673aa90e417ffecc575a5e9e3536705b
seongs <seongsland@gmail.com>
2026-01-29 15:36:14 +0900

    테스트용 log 추가

--- backend/src/main/java/com/jichan/JichanApplication.java
+++ backend/src/main/java/com/jichan/JichanApplication.java
@@ -10,6 +10,7 @@ public class JichanApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(JichanApplication.class, args);
+        System.out.println("JichanApplication START!");
     }
 
 }
